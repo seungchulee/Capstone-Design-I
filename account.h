5c5e60a1eb440edb3e0129d1b561c115fd76cf7c
seungchulee <inthetelevision@gmail.com>
2019-03-30 21:33:21 +0900

    Add files via upload

---
+++ account.h
@@ -0,0 +1,8 @@
+#include "Arduino.h"
+
+
+const char* server = "api.thingspeak.com";
+const char* myserver="18.225.6.76";
+String apiKey ="XAD1OOWWCO7290T4";
+const char* MY_SSID = "pwtop_w2";
+const char* MY_PWD = "0312233976";
